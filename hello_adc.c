f5edabb4b3e68eaddf564bc38f8a58b6571ea267
mygeone2 <113492070+mygeone2@users.noreply.github.com>
2023-10-22 12:37:47 -0300

    Update hello_adc.c

--- hello_adc.c
+++ hello_adc.c
@@ -4,27 +4,166 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
+#include <vector>
+
+
+uint32_t DEBOUNCE_TIME = 20000;
+uint32_t DEBOUNCE_TIME_ON = 2000;
+bool threshold_crossed = false;
+uint32_t start_time_crossed = 0;
+bool print_value = false; 
+bool print_time = false; 
+uint16_t THRESHOLD = 1420; // Initial threshold
+uint32_t last_crossed_time = 0;
+uint32_t start_timer_on = 0;
+uint32_t temp = 0;
+bool identified_change_on_to_off = false;
+std::vector<uint32_t> v;
+bool print_vector = false;
+bool stop_at_first_below_threshold = false;
+uint32_t end_timer_on = 0;
+uint32_t current_combination_state = 0;
+uint32_t prev_combination_state = 0;
+
+// dict with the sensor state and the time range in which it is valid
+
+    std::vector<std::pair<uint32_t, uint32_t>> sensor_ranges_measures = {
+        {25000, 30000},
+        {50000, 55000},
+        {70000, 80000},
+        {100000, 105000},
+        {126000, 129000},
+        {150000, 155000},
+        {175000, 180000},
+        {200000, 205000}
+    };
+
+
+
+
 
 int main() {
     stdio_init_all();
-    printf("ADC Example, measuring GPIO26\n");
+    std::cout << "ADC Example, measuring GPIO26\n";
 
     adc_init();
 
-    // Make sure GPIO is high-impedance, no pullups etc
     adc_gpio_init(26);
-    // Select ADC input 0 (GPIO26)
     adc_select_input(0);
 
-    while (1) {
-        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-        const float conversion_factor = 3.3f / (1 << 12);
+    std::string input_buffer;
+    
+    while (true) {
         uint16_t result = adc_read();
-        
-        printf("%f \n", result * conversion_factor);
+
+       
+
+        if (print_value) {
+            std::cout << result << std::endl;
+        }
+
+        if (result > THRESHOLD) {
+
+            uint32_t crossTime = time_us_32();
+            uint32_t difference = crossTime - last_crossed_time;
+
+            
+
+            if (print_time && difference > DEBOUNCE_TIME) {
+                //std::cout << "Time OFF: " << difference / 1000 << " ms\n";
+                //std::cout << " 0 ";
+                start_timer_on = crossTime;
+                stop_at_first_below_threshold = false;
+                
+            }
+            last_crossed_time = crossTime;
+        }else{
+            uint32_t measure_below = time_us_32();
+            uint32_t difference = measure_below - last_crossed_time;
+
+            if (print_time && difference > DEBOUNCE_TIME_ON && !stop_at_first_below_threshold) {
+                stop_at_first_below_threshold = true;
+                end_timer_on = measure_below;
+
+                uint32_t time_signal_stayed_on = end_timer_on - start_timer_on;
+                //std::cout << time_signal_stayed_on<< " ms\n";
+
+                if(time_signal_stayed_on > sensor_ranges_measures[0].first && time_signal_stayed_on < sensor_ranges_measures[0].second){
+                    current_combination_state = 1;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[1].first && time_signal_stayed_on < sensor_ranges_measures[1].second){
+                    current_combination_state = 2;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[2].first && time_signal_stayed_on < sensor_ranges_measures[2].second){
+                    current_combination_state = 3;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[3].first && time_signal_stayed_on < sensor_ranges_measures[3].second){
+                    current_combination_state = 4;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[4].first && time_signal_stayed_on < sensor_ranges_measures[4].second){
+                    current_combination_state = 5;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[5].first && time_signal_stayed_on < sensor_ranges_measures[5].second){
+                    current_combination_state = 6;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[6].first && time_signal_stayed_on < sensor_ranges_measures[6].second){
+                    current_combination_state = 7;
+                }else if(time_signal_stayed_on > sensor_ranges_measures[7].first && time_signal_stayed_on < sensor_ranges_measures[7].second){
+                    current_combination_state = 8;
+                }
+
+                if (prev_combination_state != current_combination_state) {
+                    std::cout << "Current combination state: " << current_combination_state << std::endl;
+                    prev_combination_state = current_combination_state;  // Update the previous state
+                }
+
+        }
+
+        int c = getchar_timeout_us(0);
+        if (c != PICO_ERROR_TIMEOUT) {
+            switch (c) {
+                case 's':
+                    print_value = true;
+                    print_time = false;
+                    print_vector = false;
+                    break;
+                case 'v':
+                print_vector = true;
+                print_time = false;
+                print_value = false;
+                break;
+                case 'm':
+                    print_time = true;
+                    print_value = false;
+                    print_vector = false;
+                    break;
+                case 't':
+                    getline(std::cin, input_buffer);
+                    if (input_buffer[0] == ' ') {
+                        uint16_t new_threshold = std::stoi(input_buffer.substr(1));
+                        THRESHOLD = new_threshold;
+                        std::cout << "Umbral ajustado a: " << THRESHOLD << std::endl;
+                    }
+                    break;
+                case 'd':
+                    getline(std::cin, input_buffer);
+                    if (input_buffer[0] == ' ') {
+                        uint16_t new_debounce = std::stoi(input_buffer.substr(1));
+                        DEBOUNCE_TIME = new_debounce * 1000;
+                        std::cout << "Debounce ajustado a: " << DEBOUNCE_TIME << std::endl;
+                    }
+                    break;
+                    case 'do':
+                    getline(std::cin, input_buffer);
+                    if (input_buffer[0] == ' ') {
+                        uint16_t new_debounce = std::stoi(input_buffer.substr(1));
+                        DEBOUNCE_TIME_ON = new_debounce * 1000;
+                        std::cout << "Debounce ajustado ON a: " << DEBOUNCE_TIME_ON << std::endl;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+}
 }
