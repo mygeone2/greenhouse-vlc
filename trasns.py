16b7a2e51ee85f582bf307ba3ef1e0531e1531cf
mygeone2 <113492070+mygeone2@users.noreply.github.com>
2023-11-10 13:19:20 -0300

    Update trasns.py

--- trasns.py
+++ trasns.py
@@ -1,44 +1,27 @@
-import machine
+from machine import Pin
 import utime
 import urandom
 
 # Configuración de pines PWM
-output_pin = machine.Pin(0)
-pwm = machine.PWM(output_pin)
-pwm.freq(40000)
-pwm.duty_u16(0)  # Iniciar con PWM apagado
+output_pin = Pin(0, Pin.OUT)
 
-# Función para enviar un bit usando codificación Manchester
-def send_manchester(bit):
-    half_period = 5  # Tiempo en ms para la mitad del período de un bit
-    if bit == 1:
-        pwm.duty_u16(0)
-        utime.sleep_ms(half_period)
-        pwm.duty_u16(32767)
-        utime.sleep_ms(half_period)
-    else:
-        pwm.duty_u16(32767)
-        utime.sleep_ms(half_period)
-        pwm.duty_u16(0)
-        utime.sleep_ms(half_period)
+time_cycle = 25   # ms -> 1000 us
+time_sleep = 30  # ms -> 25000 us
+update_interval = 1000  # ms (5 segundos)
+last_update = utime.ticks_ms()
+random_value = urandom.randint(1, 8)  # valor inicial
 
-try:
-    while True:
-        # Generar una trama aleatoria de 4 bits representando el estado de los 4 actuadores
-        data = [urandom.randint(0, 1) for _ in range(4)]
-        print("Enviando trama:", data)  # Imprimir la trama para depuración
-        
-        # Pausa Larga (ejemplo: 100ms) para indicar el inicio de una nueva trama
-        pwm.duty_u16(0)
-        utime.sleep_ms(100)
-        
-        for bit in data:
-            send_manchester(bit)
-        
-        # Pausa Corta (ejemplo: 10ms) entre tramas
-        pwm.duty_u16(0)
-        utime.sleep_ms(100)
+while True:
+    # Si han pasado 5 segundos desde la última actualización, genera un nuevo valor random
+    if utime.ticks_diff(utime.ticks_ms(), last_update) >= update_interval:
+        random_value = urandom.randint(1, 8)
+        print(random_value)
 
-except KeyboardInterrupt:
-    pwm.deinit()
+        last_update = utime.ticks_ms()
+
+    # Envía el pulso
+    output_pin.value(1)
+    utime.sleep_ms(time_cycle * random_value)
+    output_pin.value(0)
+    utime.sleep_ms(time_sleep)
 
